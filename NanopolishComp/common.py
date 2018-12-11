992fc2acb34a119382077b98827f668f3ea8d6e1
a-slide <aleg@ebi.ac.uk>
2018-12-11 14:43:58 +0000

    Change helper name to common and add custom exception class

--- NanopolishComp/common.py
+++ NanopolishComp/common.py
@@ -17,3 +17,8 @@ def access_file (fn, **kwargs):
     """Check if the file is readable
     """
     return os.path.isfile (fn) and os.access (fn, os.R_OK)
+
+#~~~~~~~~~~~~~~CUSTOM EXCEPTION AND WARN CLASSES~~~~~~~~~~~~~~#
+class NanopolishCompError (Exception):
+    """ Basic exception class for NanopolishComp package """
+    pass
