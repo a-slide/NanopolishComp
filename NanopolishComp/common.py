7f722d3ca8ee03bc06d4aa86b75dcc00456aa501
a-slide <aleg@ebi.ac.uk>
2019-04-22 17:05:12 +0100

    Add extra helper functions

--- NanopolishComp/common.py
+++ NanopolishComp/common.py
@@ -4,6 +4,7 @@
 # Standard library imports
 import sys
 import os
+from collections import *
 
 #~~~~~~~~~~~~~~FUNCTIONS~~~~~~~~~~~~~~#
 
@@ -13,10 +14,77 @@ def stderr_print (*args):
     sys.stderr.write(" ".join(str(a) for a in args))
     sys.stderr.flush()
 
-def access_file (fn, **kwargs):
+def file_readable (fn, **kwargs):
     """Check if the file is readable
     """
     return os.path.isfile (fn) and os.access (fn, os.R_OK)
+
+def dir_writable (fn, **kwargs):
+    """Check if the file is readable
+    """
+    if not os.path.isdir(fn):
+        fn = os.path.dirname(fn)
+    return os.path.dirname(fn) and os.access (fn, os.W_OK)
+
+def jhelp (f:"python function or method"):
+    """
+    Display a Markdown pretty help message for functions and class methods (default __init__ is a class is passed)
+    jhelp also display default values and type annotations if available.
+    Undocumented options are not displayed.
+    The docstring synthax should follow the markdown formated convention below
+    * f
+        Function or method to display the help message for
+    """
+    # For some reason signature is not always importable. In these cases the build-in help is called instead
+    try:
+        from IPython.core.display import display, Markdown, HTML
+        import inspect
+    except (NameError, ImportError) as E:
+        NanocomporeWarning ("jupyter notebook is required to use this function. Please verify your dependencies")
+        help(f)
+        return
+
+    if inspect.isclass(f):
+        f = f.__init__
+
+    if inspect.isfunction(f) or inspect.ismethod(f):
+
+        # Parse arguments default values and annotations
+        sig_dict = OrderedDict()
+        for name, p in inspect.signature(f).parameters.items():
+            sig_dict[p.name] = []
+            # Get Annotation
+            if p.annotation != inspect._empty:
+                sig_dict[p.name].append(": {}".format(p.annotation))
+            # Get default value if available
+            if p.default == inspect._empty:
+                sig_dict[p.name].append("(required)")
+            else:
+                sig_dict[p.name].append("(default = {})".format(p.default))
+
+        # Parse the docstring
+        doc_dict = OrderedDict()
+        descr = []
+        lab=None
+        for l in inspect.getdoc(f).split("\n"):
+            l = l.strip()
+            if l:
+                if l.startswith("*"):
+                    lab = l[1:].strip()
+                    doc_dict[lab] = []
+                elif lab:
+                    doc_dict[lab].append(l)
+                else:
+                    descr.append(l)
+
+        # Reformat collected information in Markdown synthax
+        s = "---\n\n**{}.{}**\n\n{}\n\n---\n\n".format(f.__module__, f.__name__, " ".join(descr))
+        for k, v in doc_dict.items():
+            s+="* **{}** *{}*\n\n{}\n\n".format(k, " ".join(sig_dict[k]), " ".join(v))
+
+        # Display in Jupyter
+        display (Markdown(s))
+
 
 #~~~~~~~~~~~~~~CUSTOM EXCEPTION AND WARN CLASSES~~~~~~~~~~~~~~#
 class NanopolishCompError (Exception):
