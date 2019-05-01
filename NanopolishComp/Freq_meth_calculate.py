265a4e5c8143e63c7af3b08d97d028a148c7934f
a-slide <aleg@ebi.ac.uk>
2019-05-01 11:01:58 +0100

    Full refactoring in OOP

--- NanopolishComp/Freq_meth_calculate.py
+++ NanopolishComp/Freq_meth_calculate.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 
+# Script inspired from https://github.com/jts/nanopolish/blob/master/scripts/calculate_methylation_frequency.py
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~IMPORTS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Standard library imports
-from collections import OrderedDict
+from collections import *
+import csv
 
 # Third party imports
-
+from tqdm import tqdm
 
 # Local imports
-from NanopolishComp.common import file_readable, dir_writable, NanopolishCompError
+from NanopolishComp.common import *
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~LOGGING INFO~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 import logging
@@ -19,22 +22,34 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 class Freq_meth_calculate():
 
     def __init__ (self,
-        output_fn:"str",
         input_fn:"str",
-        call_threshold:"float"=2.5,
-        split_groups:"bool"=False,
+        outdir:"str"="./",
+        outprefix:"str"="out",
+        min_llr:"float"=2.5,
+        min_depth:"int"=10,
+        min_meth_freq:"float"=0.05,
+        split_group:"bool"=False,
+        motif:"{cpg,gpc,dam,dcm}"="cpg",
         verbose:"bool"=False,
         quiet:"bool"=False):
         """
         Calculate methylation frequency at genomic CpG sites from the output of nanopolish call-methylation
-        * output_fn
-            Path the output reference position tsv file
         * input_fn
             Path to a nanopolish call_methylation tsv output file
-        * call_threshold
+        * outdir
+            Path to the output folder
+        * outprefix
+            text outprefix for all the files generated
+        * min_llr
             Log likelihood ratio threshold
-        * split_groups
-             split multi-cpg groups
+        * min_depth
+            Minimal number of reads covering a site to be reported
+        * min_meth_freq
+            Minimal methylation frequency of a site to be reported
+        * split_group
+            If True, multi motif groups (sequence with close motifs) are split in individual site
+        * motif
+            Methylation motif type
         * verbose
             Increase verbosity
         * quiet
@@ -45,110 +60,209 @@ class Freq_meth_calculate():
         self.log = logging.getLogger()
         if verbose:
             self.log.setLevel (logging.DEBUG)
-            self.verbose_level = 2
         elif quiet:
             self.log.setLevel (logging.WARNING)
-            self.verbose_level = 0
-        else:
-            self.log.setLevel (logging.INFO)
-            self.verbose_level = 1
 
         # Verify parameters validity
-        self.log.info ("Checking arguments")
-        self.log.debug("Test input file readability")
+        self.log.warning ("## Checking arguments ##")
+        self.log.info("Test input file readability")
         if input_fn != 0 and not file_readable (input_fn):
             raise IOError ("Cannot read input file")
-        self.log.debug("\tTesting output file writability")
-        if not dir_writable (output_fn):
+        self.log.info("Testing output dir writability")
+        if not dir_writable (outdir):
             raise IOError ("Cannot write output file in indicated folder. Create the output folder if it does not exist yet")
 
-        # Save args to self values
-        self.output_fn = output_fn
-        self.input_fn = input_fn
-        self.call_threshold = call_threshold
-        self.split_groups = split_groups
+        if motif == "cpg":
+            motif_seq = "CG"
+        elif motif == "gpc":
+            motif_seq = "GC"
+        elif motif == "dam":
+            motif_seq = "GATC"
+        elif motif == "dcm":
+            motif_seq = "CCAGG"
+        else:
+            raise NanopolishCompError("Not a valid motif: {}".format(motif))
 
+        # init object counters
+        self.site_c = Counter()
+        self.pos_c = Counter()
 
-#
-# #! /usr/bin/env python
-#
-# import sys
-# import csv
-# import argparse
-#
-#
-# class SiteStats:
-#     def __init__(self, g_size, g_seq):
-#         self.num_reads = 0
-#         self.called_sites = 0
-#         self.called_sites_methylated = 0
-#         self.group_size = g_size
-#         self.sequence = g_seq
-#
-# def Freq_meth_calculate
-#
-#
-# def update_call_stats(key, num_called_cpg_sites, is_methylated, sequence):
-#     if key not in sites:
-#         sites[key] = SiteStats(num_called_cpg_sites, sequence)
-#
-#     sites[key].num_reads += 1
-#     sites[key].called_sites += num_called_cpg_sites
-#     if is_methylated > 0:
-#         sites[key].called_sites_methylated += num_called_cpg_sites
-#
-# parser = argparse.ArgumentParser( description='Calculate methylation frequency at genomic CpG sites')
-# parser.add_argument('-c', '--call-threshold', type=float, required=False, default=2.5)
-# parser.add_argument('-i', '--input', type=str, required=False)
-# parser.add_argument('-s', '--split-groups', action='store_true')
-# args = parser.parse_args()
-# assert(args.call_threshold is not None)
-#
-# sites = dict()
-#
-# if args.input:
-#     in_fh = open(args.input)
-# else:
-#     in_fh = sys.stdin
-# csv_reader = csv.DictReader(in_fh, delimiter='\t')
-#
-# for record in csv_reader:
-#
-#     num_sites = int(record['num_motifs'])
-#     llr = float(record['log_lik_ratio'])
-#
-#     # Skip ambiguous call
-#     if abs(llr) < args.call_threshold:
-#         continue
-#     sequence = record['sequence']
-#
-#     is_methylated = llr > 0
-#
-#     # if this is a multi-cpg group and split_groups is set, break up these sites
-#     if args.split_groups and num_sites > 1:
-#         c = str(record['chromosome'])
-#         s = int(record['start'])
-#         e = int(record['end'])
-#
-#         # find the position of the first CG dinucleotide
-#         sequence = record['sequence']
-#         cg_pos = sequence.find("CG")
-#         first_cg_pos = cg_pos
-#         while cg_pos != -1:
-#             key = (c, s + cg_pos - first_cg_pos, s + cg_pos - first_cg_pos)
-#             update_call_stats(key, 1, is_methylated, "split-group")
-#             cg_pos = sequence.find("CG", cg_pos + 1)
-#     else:
-#         key = (str(record['chromosome']), int(record['start']), int(record['end']))
-#         update_call_stats(key, num_sites, is_methylated, sequence)
-#
-# # header
-# print("\t".join(["chromosome", "start", "end", "num_motifs_in_group", "called_sites", "called_sites_methylated", "methylated_frequency", "group_sequence"]))
-#
-# sorted_keys = sorted(sites.keys(), key = lambda x: x)
-#
-# for key in sorted_keys:
-#     if sites[key].called_sites > 0:
-#         (c, s, e) = key
-#         f = float(sites[key].called_sites_methylated) / sites[key].called_sites
-#         print("%s\t%s\t%s\t%d\t%d\t%d\t%.3f\t%s" % (c, s, e, sites[key].group_size, sites[key].called_sites, sites[key].called_sites_methylated, f, sites[key].sequence))
+        self.log.warning ("## Parsing methylation_calls file ##")
+        sites_d = self._parse_methylation_calls (
+            fn = input_fn,
+            min_llr = min_llr,
+            min_depth = min_depth,
+            min_meth_freq = min_meth_freq,
+            split_group = split_group,
+            motif_seq = motif_seq)
+
+        if sites_d:
+            self.log.warning ("## Write output files ##")
+            self._write_output (sites_d=sites_d, outdir=outdir, outprefix=outprefix)
+        else:
+            self.log.warning("No valid site found in the imput file")
+
+    def _parse_methylation_calls(self, fn, min_llr=2.5, min_depth=10, min_meth_freq=0.05, split_group=False, motif_seq="CG"):
+        """"""
+        # Create collection to store results
+        self.log.info ("Starting to parse file methylation_calls file")
+        sites_d = OrderedDict()
+        with open (fn) as fp:
+            for line_d in tqdm(csv.DictReader(fp, delimiter='\t'), unit=" lines", disable=self.log.level>=30):
+                line_d = numeric_cast_dict (line_d)
+
+                # Update counter if verbose only
+                self.site_c["total"] +=1
+                if line_d["log_lik_ratio"] >= min_llr:
+                    self.site_c["methylated"] +=1
+                elif line_d["log_lik_ratio"] <= min_llr:
+                    self.site_c["unmethylated"] +=1
+                else :
+                    self.site_c["ambiguous"] +=1
+
+                # In case multi motif group have to be split
+                if line_d["num_motifs"] > 1 and split_group:
+
+                    # Find all index corresponding to the motif
+                    for pos in find_subseq_index (line_d["sequence"], motif_seq):
+                        start = line_d["start"]+pos-5
+                        seq = line_d["sequence"][pos-5:pos+6]
+
+                        # Create entry in sites dict if it does not already exist
+                        coord_key = "{}_{}_{}".format(line_d["chromosome"], start, line_d["strand"])
+                        if not coord_key in sites_d:
+                            sites_d[coord_key] = Site(seq, line_d["num_motifs"], line_d["chromosome"], start , start+1, line_d["strand"], min_llr=min_llr)
+                        sites_d[coord_key].update(line_d["log_lik_ratio"])
+
+                # Otherwise
+                else:
+                    # Create entry in sites dict if it does not already exist
+                    coord_key = "{}_{}_{}".format(line_d["chromosome"], line_d["start"], line_d["strand"])
+                    if not coord_key in sites_d:
+                        sites_d[coord_key] = Site(line_d["sequence"], line_d["num_motifs"], line_d["chromosome"], line_d["start"] , line_d["end"]+1, line_d["strand"], min_llr=min_llr)
+                    sites_d[coord_key].update(line_d["log_lik_ratio"])
+
+        # Print read level counter summary
+        self.log.debug ("Read sites summary")
+        self.log.debug (counter_to_str(self.site_c))
+
+        self.log.info ("Filtering out positions with low coverage or methylation frequency")
+        filtered_sites_d = OrderedDict()
+        for coord_key, site in tqdm(sites_d.items(), unit=" positions", disable=self.log.level>=30):
+
+            # Update counter
+            self.pos_c["total"] +=1
+            if site.valid_call_reads < min_depth:
+                self.pos_c["low_coverage"]+=1
+            elif site.meth_freq < min_meth_freq:
+                self.pos_c["low_meth_freq"]+=1
+            else:
+                self.pos_c["valid"] +=1
+
+            if site.valid_call_reads >= min_depth and site.meth_freq >= min_meth_freq:
+                filtered_sites_d[coord_key] = site
+
+        # Print genomic positions level counter summary
+        self.log.debug ("Genomic positions summary")
+        self.log.debug (counter_to_str(self.pos_c))
+
+        return filtered_sites_d
+
+    def __repr__ (self):
+        m = ""
+        m+="Read sites summary\n"
+        m+=counter_to_str(self.site_c)
+        m+="Genomic positions summary\n"
+        m+=counter_to_str(self.pos_c)
+        return m
+
+    def _write_output (self, sites_d, outdir, outprefix):
+        """"""
+        # Bed file output
+        self.log.info("Writing bed file")
+        fn = os.path.join(outdir, outprefix+"_freq_meth_calculate.bed")
+        with open (fn, "w") as fp:
+            # Write header
+            fp.write (Site.BED_header(outprefix)+"\n")
+            # Sort by coordinates and write values
+            for site in sorted(sites_d.values(), key=lambda s: (s.chrom, s.start, s.strand)):
+                fp.write (site.to_bed()+"\n")
+
+        # TSV file output
+        self.log.info("Writing tsv file")
+        fn = os.path.join(outdir, outprefix+"_freq_meth_calculate.tsv")
+        with open (fn, "w") as fp:
+            # Write header
+            fp.write (Site.TSV_header()+"\n")
+            # Sort by methylation frequency and write values
+            for site in sorted(sites_d.values(), key=lambda s: s.meth_freq, reverse=True):
+                fp.write (site.to_tsv()+"\n")
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~HELPER CLASS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+class Site ():
+    """Structure like class to store site information"""
+
+    # Class id dispatcher
+    ID = 0
+    @classmethod
+    def next_id (cls):
+        id = cls.ID
+        cls.ID +=1
+        return id
+
+    @classmethod
+    def BED_header (cls, track_name):
+        return "track name='{}' description='Methylation frequency track generated with NanopolishComp' useScore=1".format(track_name)
+
+    @classmethod
+    def TSV_header (cls):
+        return "\t".join(["chrom","start","end","strand","site_id","methylated_reads","unmethylated_reads","ambiguous_reads","sequence","num_motifs","meth_freq"])
+
+    def __init__ (self, seq, num_motifs, chrom, start, end, strand, min_llr=2.5):
+        """"""
+        self.seq = seq
+        self.num_motifs = num_motifs
+        self.chrom = chrom
+        self.start = start
+        self.end = end
+        self.strand = strand
+        self.methylated_reads = 0
+        self.unmethylated_reads = 0
+        self.ambiguous_reads = 0
+        self.min_llr = min_llr
+        self.id = self.next_id()
+
+    def __repr__(self):
+        return "{}:{}-{}({}) / id:{} / reads:{} / meth_freq:{:03}".format(
+            self.chrom, self.start, self.end, self.strand, self.id, self.valid_call_reads, self.meth_freq)
+
+    @property
+    def valid_call_reads (self):
+        return self.methylated_reads+self.unmethylated_reads
+
+    @property
+    def meth_freq (self):
+        if self.valid_call_reads:
+            return self.methylated_reads/self.valid_call_reads
+        else:
+            return 0
+
+    def update (self, llr):
+        """"""
+        if llr >= self.min_llr:
+            self.methylated_reads +=1
+        elif llr <= -self.min_llr:
+            self.unmethylated_reads +=1
+        else:
+            self.ambiguous_reads +=1
+
+    def to_bed (self):
+        """"""
+        return "{}\t{}\t{}\t{}\t{:06}\t{}".format(
+            self.chrom, self.start, self.end, self.id ,int(self.meth_freq*1000), self.strand)
+
+    def to_tsv (self):
+        """"""
+        return "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{:.6f}".format(
+            self.chrom, self.start, self.end, self.strand, self.id, self.methylated_reads, self.unmethylated_reads, self.ambiguous_reads, self.seq, self.num_motifs, self.meth_freq)
