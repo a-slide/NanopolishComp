04afd2256a8b12ce97680d4cf3cc5ede8bab58d6
a-slide <aleg@ebi.ac.uk>
2019-05-08 14:15:30 +0100

    Bump version number

--- NanopolishComp/__init__.py
+++ NanopolishComp/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Define self package variable
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 __description__ = 'NanopolishComp is a Python3 package for downstream analyses of Nanopolish output files'
 
 # Collect info in a dictionary for setup.py
