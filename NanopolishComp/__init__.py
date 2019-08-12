a999ed6976a9e9be6f59cc99b637e73e852f0130
a-slide <aleg@ebi.ac.uk>
2019-08-12 18:34:18 +0100

    Update setup and init and bump up version number

--- NanopolishComp/__init__.py
+++ NanopolishComp/__init__.py
@@ -1,26 +1,5 @@
 # -*- coding: utf-8 -*-
 
 # Define self package variable
-__version__ = "0.6.2"
+__version__ = "0.6.4"
 __description__ = 'NanopolishComp is a Python3 package for downstream analyses of Nanopolish output files'
-
-# Collect info in a dictionary for setup.py
-setup_dict = {
-    "name": __name__,
-    "version": __version__,
-    "description": __description__,
-    "url": "https://github.com/a-slide/NanopolishComp",
-    "author": 'Adrien Leger',
-    "author_email": 'aleg@ebi.ac.uk',
-    "license": "MIT",
-    "python_requires":'>=3.3',
-    "classifiers": [
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3'],
-    "install_requires": ['numpy>=1.14.0', 'tqdm>=4.23.4'],
-    "packages": [__name__],
-    "entry_points":{'console_scripts': [
-        'NanopolishComp=NanopolishComp.__main__:main']}}
--- setup.py
+++ setup.py
@@ -2,5 +2,32 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
-from NanopolishComp import setup_dict
-setup(**setup_dict)
+
+# Define package info
+name = "NanopolishComp"
+version = "0.6.4"
+description = 'NanopolishComp is a Python3 package for downstream analyses of Nanopolish output files'
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# Collect info in a dictionnary for setup.py
+setup(
+    name = name,
+    description = description,
+    version = version,
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/a-slide/NanopolishComp",
+    author = 'Adrien Leger',
+    author_email = 'aleg@ebi.ac.uk',
+    license= "MIT",
+    python_requires ='>=3.6',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'],
+    install_requires = ['numpy>=1.14.0', 'tqdm>=4.23.4'],
+    packages = [name],
+    entry_points = {'console_scripts': ['NanopolishComp=NanopolishComp.__main__:main']})
